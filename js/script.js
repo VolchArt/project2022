c21593c92fd0825d0a196b97b2fed31eae9d7f73
Artemiy <temkavolch@gmail.com>
2022-06-14 23:38:58 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,64 @@
+// "use strict"; Уроки 7 - 10
+
+// a = 15;
+// console.log(a);
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// console.log(name);
+// var name = 'Ivan';
+
+// {
+//     let result = 50;
+// }
+
+// console.log(result);
+
+// "use strict"; Урок 12 упражнение
+
+// let storeName = "SimpleName";
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ["NameOne","NameTwo","NameThree"],
+//     products: {
+//         productOne: 5000,
+//         productTwo: 4000,
+//     },
+//     open: true
+// };
+
+// "use strict"; Урок 13 самостоятельная работа
+
+// const answers = [];
+// answers [0] = prompt ("Как ваше имя", " ");
+// answers [1] = +prompt ("Сколько вам лет", " ");
+// answers [2] = prompt ("Введите желаемое имя пользователя", " ");
+
+// let ageSUM = answers[1] + 5;
+// let result = alert( answers[0] + ", Ваше предполагаемое имя: " + answers[2] + ageSUM);
+
+// console.log(result);
+
+// "use strict"; //Урок 14 самостоятельная работа (интерволяция в 62 строчке)
+
+// const answers = [];
+// answers [0] = prompt ("Как ваше имя", " ");
+// answers [1] = +prompt ("Сколько вам лет", " ");
+// answers [2] = prompt ("Введите желаемое имя пользователя", " ");
+
+// let ageSUM = answers[1] + 5;
+// let result = alert( `${answers[0]}, Ваше предполагаемое имя: ${answers[2] + ageSUM}`);
+
+// console.log(result);
