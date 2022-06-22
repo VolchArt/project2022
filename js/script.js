412f1a69d23fbbb6e129de31d43f8c852213391b
Artemiy <temkavolch@gmail.com>
2022-06-22 23:06:59 +0300

    Практика с *, задание 2

--- js/script.js
+++ js/script.js
@@ -216,3 +216,38 @@
 //     result += '\n';
 // }
 // console.log(result);
+
+//Практика с *
+
+//ЗАДАНИЕ1
+// "use strict";
+
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+
+// for (let i = 0; i<arr.length; i++) {
+//     result[i]=arr[i];
+// }
+// console.log(result);
+
+//ЗАДАНИЕ2
+"use strict";
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+for (let i = 0; i<data.length; i++) {
+    if (typeof data[i] === 'string') {
+        data[i] += ' - done';
+    } else {
+    data[i]= data[i] * 2;
+    }
+}
+console.log(data);
+//ЭКСПЕРИМЕНТЫ
+// let a='2';
+// const arr =  [0,'2'];
+
+// if (a && arr[1] == true) {
+//     console.log ('Ура!');
+// } else {
+//     console.log (typeof arr[1], typeof a );
+// }
